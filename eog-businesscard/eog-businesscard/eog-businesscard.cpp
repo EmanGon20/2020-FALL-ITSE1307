617878999d13773b3a7313b9298ee10056a665eb
EmanGon20 <60245928+EmanGon20@users.noreply.github.com>
2020-02-06 20:10:32 -0600

    Add files via upload

---
+++ eog-businesscard/eog-businesscard/eog-businesscard.cpp
@@ -0,0 +1,26 @@
+// eog-businesscard.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Welcome to ITSE 1307!\n";
+	std::cout << "54th Street\n";// Business Title
+	std::cout << "210-684-0054\n";// Business Phone Number
+	std::cout << "5722 W. Loop 1604 N., San Antonio, TX 78251\n";// Business Address
+	std::cout << "https://www.54thstreetgrill.com/san-antonio-tx-culebra.html \n";// Business Website URL
+	std::cout << "Info54@54thStreetGrill.com\n";// Business Email
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
